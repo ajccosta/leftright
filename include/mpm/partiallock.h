82c7fc8ac6402bfb79588a5bba145ea6f40f4435
ajccosta <aj.costa@campus.fct.unl.pt>
2024-04-10 16:42:01 +0100

    added partial lock implementation

---
+++ include/mpm/partiallock.h
@@ -0,0 +1,71 @@
+#include <atomic>
+#include <cassert>
+
+//Find out better way to implement this
+thread_local uint64_t my_turn;
+
+namespace pl
+{
+
+    class PartialLock
+    {
+        private:
+            std::atomic_uint_fast64_t ticket{0};
+            std::atomic_uint_fast64_t turn{0};
+    
+        public: 
+            PartialLock() = default;
+            ~PartialLock();
+    
+            enum LockType
+            {
+               FULL = 1,
+               PARTIAL,
+            };
+    
+    	    LockType
+            lock()
+            {
+                my_turn = ticket.fetch_add(2, std::memory_order_seq_cst);
+                uint64_t t;
+                do {
+                     t = turn.load(std::memory_order_seq_cst);
+                } while(t != my_turn && (t + 1) != my_turn);
+                return t == my_turn ? FULL : PARTIAL;
+            } 
+    
+    	    void
+            unlock_full()
+            {
+                turn.fetch_add(2, std::memory_order_seq_cst);
+            } 
+    
+    	    void
+            unlock_partial()
+            {
+                turn.fetch_add(1, std::memory_order_seq_cst);
+            }
+    
+            void
+            waitfor_partial()
+            {
+                while(turn.load(std::memory_order_seq_cst) != my_turn)
+                {
+                    std::this_thread::yield();
+                }
+            }
+    
+            bool
+            writers_in_flight()
+            {
+                return ticket.load(std::memory_order_seq_cst) > my_turn;
+            }
+    
+            bool
+            am_i_partial()
+            {
+                return true;
+            }
+    }; 
+
+}
